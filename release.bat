e6dd61e40a5ff2935979844ff520a33a00322d1f
Fekete Agnes <Fekete.Agnes@prolan.hu>
2018-04-20 17:05:19 +0200

    d

--- release.bat
+++ release.bat
@@ -1,3 +1,1 @@
-call mvn release:prepare -DallowTimestampedSnapshots=true -DautoVersionSubmodules=true -DpushChanges=false -DupdateDependencies=true -DtagBase=scm:git:file://C:/users/fekete.agnes/my_github/test/test
-
-call mvn release:perform -DconnectionUrl=scm:git:file://C:/users/fekete.agnes/my_github/test/test
+call mvn unleash:perform
